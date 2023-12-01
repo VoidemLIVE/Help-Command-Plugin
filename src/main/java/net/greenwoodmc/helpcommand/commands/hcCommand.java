bca4740d468a0a59dc62f2918163e32f1cd25d08
Callum <81859284+VoidemLIVE@users.noreply.github.com>
2023-12-01 23:39:49 +0000

    WHY DID THE RELOAD COMMAND DECIDE TO NOT WORK?????
    Fixed :)

--- src/main/java/net/greenwoodmc/helpcommand/commands/hcCommand.java
+++ src/main/java/net/greenwoodmc/helpcommand/commands/hcCommand.java
@@ -5,10 +5,13 @@ import net.greenwoodmc.helpcommand.util.TextUtil;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.stream.Collectors;
 
 public class hcCommand implements CommandExecutor {
@@ -31,6 +34,13 @@ public class hcCommand implements CommandExecutor {
                 if (arg1.equalsIgnoreCase("reload")) {
                     ver = config.getString("reload");
                     plug.reloadConfig();
+                    try {
+                        config.load(new File(plug.getDataFolder(), "config.yml"));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    } catch (InvalidConfigurationException e) {
+                        throw new RuntimeException(e);
+                    }
                     if (isPlayer) {
                         sender.sendMessage(TextUtil.color(ver));
                     } else {
