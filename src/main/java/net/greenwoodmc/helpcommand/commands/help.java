31c2ced37f7c7692d61cdb02a43c28606131dc3f
Callum <voidemtwitch@gmail.com>
2024-09-28 21:55:02 +0100

    V2.9 Upgraded page prompt

--- src/main/java/net/greenwoodmc/helpcommand/commands/help.java
+++ src/main/java/net/greenwoodmc/helpcommand/commands/help.java
@@ -6,12 +6,15 @@ import net.greenwoodmc.helpcommand.util.TextUtil;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,11 +32,13 @@ public class help implements CommandExecutor {
         String ver;
         String ver2;
         String pageText = config.getString("pagePrompt.page");
+        Pattern pattern = Pattern.compile("\\$\\[(.*?)\\]");
+        Matcher matcherPageText = pattern.matcher(pageText);
+        Matcher matcherPrevPage = pattern.matcher(config.getString("pagePrompt.arrowBack"));
+        Matcher matcherNextPage = pattern.matcher(config.getString("pagePrompt.arrowForward"));
+        String backArrow = config.getString("pagePrompt.arrowBack");
+        String forwardArrow = config.getString("pagePrompt.arrowForward");
         int currPageNumber;
-        String colour1 = config.getString("pagePrompt.pageNumberColour");
-        String colour2 = config.getString("pagePrompt.pageTextColour");
-        ChatColor COLOUR1 = ChatColor.valueOf(colour1);
-        ChatColor COLOUR2 = ChatColor.valueOf(colour2);
         List<Integer> enabledPages = config.getIntegerList("pagesEnabled");
         int lastPage = enabledPages.get(enabledPages.size() - 1);
         JavaPlugin plug = JavaPlugin.getPlugin(HelpCommand.class);
@@ -62,26 +67,55 @@ public class help implements CommandExecutor {
                         if (pagePromptEnabled && enabledPages.size() > 1) { // Only show page prompt if more than one page
                             ComponentBuilder pagePromptBuilder = new ComponentBuilder();
 
+
                             if (currPageNumber > 1) { // Show previous page arrow if not on the first page
                                 int previousPageNumber = currPageNumber - 1;
-                                pagePromptBuilder.append("[<< " + previousPageNumber + "] ")
-                                        .color(COLOUR1)
-                                        .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/help " + previousPageNumber));
+
+                                if (matcherPrevPage.find()) {
+                                    String inside = matcherPrevPage.group(1);
+                                    if ("prevNum".equalsIgnoreCase(inside)) {
+                                        backArrow = backArrow.replace("$[prevNum]", String.valueOf(previousPageNumber));
+                                        pagePromptBuilder.append(TextUtil.color(backArrow + " "))
+                                                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/help " + previousPageNumber));
+                                    }
+                                } else {
+                                    pagePromptBuilder.append(TextUtil.color(backArrow + " "))
+                                            .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/help " + previousPageNumber));
+                                }
                             }
 
-                            // Add the current page number without a click event
-                            pagePromptBuilder.append(pageText + " " + currPageNumber)
-                                    .color(COLOUR2);
+                            TextComponent pageTextComponent = null;
+                            if (matcherPageText.find()) {
+                                String inside = matcherPageText.group(1);
+                                if ("pageNum".equalsIgnoreCase(inside)) {
+                                    pageText = pageText.replace("$[pageNum]", String.valueOf(currPageNumber));
+                                    pageTextComponent = new TextComponent(TextUtil.color(pageText));
+                                }
+                            } else {
+                                pageTextComponent = new TextComponent(TextUtil.color(pageText));
+                            }
+
+                            pagePromptBuilder.append(pageTextComponent);
 
                             if (currPageNumber < lastPage) { // Show next page arrow if not on the last page
                                 int nextPageNumber = currPageNumber + 1;
-                                pagePromptBuilder.append(" [" + nextPageNumber + " >>]")
-                                        .color(COLOUR1)
-                                        .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/help " + nextPageNumber));
+
+                                if (matcherNextPage.find()) {
+                                    String inside = matcherNextPage.group(1);
+                                    if ("nextNum".equalsIgnoreCase(inside)) {
+                                        forwardArrow = forwardArrow.replace("$[nextNum]", String.valueOf(nextPageNumber));
+                                        pagePromptBuilder.append(" " + TextUtil.color(forwardArrow))
+                                                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/help " + nextPageNumber));
+                                    }
+                                } else {
+                                    pagePromptBuilder.append(" " + TextUtil.color(forwardArrow))
+                                            .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/help " + nextPageNumber));
+                                }
                             }
 
                             player.spigot().sendMessage(pagePromptBuilder.create());
                         }
+
                     } else {
                         player.sendMessage(TextUtil.color(config.getString("pageNA")));
                     }
